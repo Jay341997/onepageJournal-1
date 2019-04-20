d48f61d19cedd6842791b9f9d8b4c5bfb9d18be0
Dhruv34788 <dhruv.barochia34788@gmail.com>
2019-04-20 18:42:59 +0530

    added grid to state

--- src/components/grid.jsx
+++ src/components/grid.jsx
@@ -10,13 +10,13 @@ export class Grid extends Component {
       initDate : '15.05.1998',
       events : [
         {
-          id : 'a',
           name : 'hello a',
           date : '14.04.2018',
           color : 'red' ,
           week : 5
         }
       ],
+      grid : []
     }
   }
   whichWeek(date){ 
@@ -24,41 +24,48 @@ export class Grid extends Component {
     let thisDate = new Date(date.replace(pattern,'$3-$2-$1'));
     return Math.round((thisDate - this.state.baseDate) / (7 * 24 * 60 * 60 * 1000));
   }
-  init(){
-    //this.initDate();
-    //this.initGrid();
-    //this.initEvents();
+
+  componentWillMount(){
+    let i
+    let newgrid= []
+    for(i=1;i<=5200;i++){
+      let eventbox = {
+        name : '',
+        date : '',
+        week : i,
+        color : ''
+      }
+      if(i%52 === 1){
+        eventbox.color = 'green'
+      }
+      newgrid.push(eventbox)
+    }
+
+    this.state.events.forEach(event => {
+      newgrid[event.week] = event
+      console.log(newgrid)
+      this.setState({
+        grid : newgrid
+      });
+    });
   }
 
-  initGrid(){
-    let i
-    let boxes= []
-    for(i=1;i<=10;i++){
-      //add past & future
-      //add birthdays
-      let eventbox = <Eventbox key={i}/>
-      boxes.push(eventbox)
-    }
-    return boxes
-  }
-  initEvents(){
-    let boxes = []
-    this.state.events.forEach(event => {
-      console.log(event)
-      let eventbox = <Eventbox name={event.name} date={event.date} color={event.color} key={event.week}/>
-      boxes.push(eventbox)
-    });
-    console.log(boxes)
-    return boxes
-  }
   render() {
     return (
       <div className="grid-box">
-        {this.initGrid()}
-        {this.initEvents()}
+      {this.state.grid.map((item) => 
+        <Eventbox key={item.week} date={item.date} color={item.color}/>
+      )}
       </div>
     )
   }
 }
 
 export default Grid
+
+
+/* To-Do
+- formatting according to  1) past/future 2) birthdays
+- fetch data from DB
+- 
+*/
