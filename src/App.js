4136da351611cdef51db5e4abeac9fec5e17118d
Dhruv34788 <dhruv.barochia34788@gmail.com>
2019-04-22 10:52:42 +0530

    added header class

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
 import React, { Component } from 'react';
 import Grid from './components/grid'
+import AddEvent from './containers.js/addevent'
 import './App.css';
 
+
 class App extends Component {
+  
   render() {
     return (
       <div className="App">
-        <h1>One-Page Journal</h1>
+        <div className='header'>
+          <h1>One-Page Journal</h1>
+          <AddEvent/>
+        </div>
         <Grid/>
       </div>
     );
