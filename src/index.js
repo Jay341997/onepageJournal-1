e5e85850997e1e00c9ba43f0792ab0b77a2b8e90
Dhruv34788 <dhruv.barochia34788@gmail.com>
2019-04-28 03:13:19 +0530

    added rounter element

--- src/index.js
+++ src/index.js
@@ -1,23 +1,49 @@
-import React from 'react';
+import React from 'react'
 import { render } from 'react-dom'
+import { BrowserRouter as Router, Route } from 'react-router-dom'
+//redux-related imports
 import { Provider } from 'react-redux'
 import { createStore } from 'redux'
 import rootReducer from './reducers'
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
-
+//pages
+import Registerpage from './pages/registerpage';
+import Homepage from './pages/homepage'
+//styles
+import './index.css'
+import * as serviceWorker from './serviceWorker'
 
 const store= createStore(rootReducer)
 
 render(
   <Provider store={store}>
-    <App/>
+    <Router>
+      <Route path="/register" component={Registerpage} />
+      <Route path="/home" component={Homepage} />
+  </Router>
   </Provider>
-  ,document.getElementById('root')
+  , document.getElementById('root')
 )
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+serviceWorker.register()
+/*
+import { ApolloProvider } from 'react-apollo'
+import { ApolloClient } from 'apollo-client'
+import { createHttpLink } from 'apollo-link-http'
+import { InMemoryCache } from 'apollo-cache-inmemory'
+
+const httpLink = createHttpLink({
+  uri: 'http://localhost:4000'
+})
+
+const client = new ApolloClient({
+  link: httpLink,
+  cache: new InMemoryCache()
+})
+
+render(
+    <ApolloClient client={client}>
+    <App/>
+  </ApolloClient>
+  ,document.getElementById('root')
+)
+*/
