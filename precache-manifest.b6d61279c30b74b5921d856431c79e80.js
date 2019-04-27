0a988973527ef3ef848c33cceeb6e2eb7739d3db
dbarochiya <dhruv.barochia34788@gmail.com>
2019-04-28 03:51:15 +0530

    Updates

---
+++ precache-manifest.b6d61279c30b74b5921d856431c79e80.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "77b552b14f39cd311e12",
+    "url": "/1pagejournal/static/css/main.4a3fd673.chunk.css"
+  },
+  {
+    "revision": "77b552b14f39cd311e12",
+    "url": "/1pagejournal/static/js/main.88e01382.chunk.js"
+  },
+  {
+    "revision": "ffb3072f96f3d629aa1d",
+    "url": "/1pagejournal/static/js/runtime~main.1cf43820.js"
+  },
+  {
+    "revision": "6f0557643cd1b84210c1",
+    "url": "/1pagejournal/static/js/2.98d0f5e4.chunk.js"
+  },
+  {
+    "revision": "310c9116be1846ed7a90e21b32775379",
+    "url": "/1pagejournal/index.html"
+  }
+];
