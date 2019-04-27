1ebbab27d56029c0bf46ae96d4551b14e01385b7
Dhruv34788 <dhruv.barochia34788@gmail.com>
2019-04-28 03:16:40 +0530

    new page : registration

---
+++ src/pages/registerpage.jsx
@@ -0,0 +1,30 @@
+import React, { Component } from 'react';
+import {Redirect} from 'react-router-dom';
+import RegisterForm from '../components/registerform'
+import './infopage.css';
+
+class Infopage extends Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      redirecttohome : false
+    }
+  }
+  redirect = () => {
+    this.setState({
+      redirecttohome : true
+    })
+  }
+  render() {
+    if(this.state.redirecttohome === true){
+      return <Redirect to='/home'/>
+    }
+    return (
+      <div className="register-page">
+        <RegisterForm redirect={this.redirect}/>
+      </div>
+    );
+  }
+}
+
+export default Infopage;
